8b9948c37151efd33bc93c0d1dd0e10f867582e4
oadm <oadm@ubuntu.corp.digital-grass.ru>
2016-09-27 17:23:15 +0300

    Event creator module. Start extraction from files.

--- modules/event/index.php
+++ modules/event/index.php
@@ -1,3 +1,47 @@
 <?php
+require_once('/var/www/html/classes/connect.php');
+
 $id_event=$argv[1];
-echo $id_event;
+//echo $id_event;
+//Получаем все данные о событии.
+$sql_event_info="select * from `events` where `id`='$id_event'";
+$event_info=new Connection($sql_event_info);
+$info=$event_info->Connect();
+$event_start_time=$info['0']['start_time'];
+$event_end_time=$info['0']['end_time'];
+$id_monitor=$info['0']['monitor_id'];
+//получаем время начала записи с камеры и настоящее
+$sql_start_rec="select * from `log_record` where `id_monitor`='$id_monitor'";
+$start_rec=new Connection($sql_start_rec);
+$rec=$start_rec->Connect();
+$s_rec=$rec['0']['start_time'];
+$event_start_time_s=  strtotime($event_start_time);
+$s_rec_s=strtotime($s_rec);
+if($event_start_time_s<$s_rec_s){
+    //ffmpeg с олд
+    //ff с текущей
+    //сплит
+}else{
+    //фф с текущей
+}
+
+/*$n_time=date("Y-m-d H:i:s");
+$delta_rec=strtotime($n_time)-strtotime($s_rec);
+$delta_ev=;*/
+
+/*
+//Если часы в начале и конце разные, то будем использовать сплит из старой записи и текущей.
+$event_start_time_s=  strtotime($event_start_time);
+$event_end_time_s=  strtotime($event_end_time);
+$event_start_time=getdate($event_start_time_s);
+$event_end_time=getdate($event_end_time_s);
+$event_start_h=$event_start_time['hours'];
+$event_end_h=$event_end_time['hours'];
+if($event_start_h!=$event_end_h){
+ 
+    //ffmpeg с олд
+    //ff с текущей
+    //сплит
+}else{
+    //фф с текущей
+}*/
--- test.php
+++ test.php
@@ -1,17 +1,13 @@
 <?php
-require_once ('/var/www/html/configs/database.config');
-
-
-$connect= mysqli_connect(HOST,USER,PASS,DB) or die (mysqli_error());
-//mysqli_select_db(DB,$connect);
-//mysqli_set_charset('utf8');
-$qu=mysqli_query($connect,"select * from `test`");
-print_r($qu);
-var_dump($qu);
-if($qu==bool){
-    echo 'si';
-}else{
-    print_r($qu);
-}
-//insert into `test` (`id`,`val`) values ('2','2')
-//update `test` set `val`='3' where `id`='1'
+$d1="2016-09-27 14:59:40";
+$d2="2016-09-27 15:00:10";
+$D1=  strtotime($d1);
+$D2= strtotime($d2);
+$dd1=getdate($D1);
+$dd2=getdate($D2);
+echo $dd1['hours'].'/';
+echo $dd2['hours'];
+//$d3=$D2-$D1;
+//echo $d3;
+//echo $D1.'/';
+//echo $D2;
