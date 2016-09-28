f42d33312bf6d795365c978b7ded11fd8a219f34
oadm <oadm@ubuntu.corp.digital-grass.ru>
2016-09-28 14:53:51 +0300

    Event creator module. Tested beta.

--- modules/event/index.php
+++ modules/event/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once('/var/www/html/classes/connect.php');
 require_once('/var/www/html/configs/path.config');
+require_once('/var/www/html/configs/interval.config');
 $id_event=$argv[1];
 $cam_name=$argv[2];
 //echo $id_event;
@@ -27,17 +28,26 @@ $info=$monitor_info->Connect();
 $path_monitor=$info['0']['path'];
 $cam_name=$info['0']['name'];*/
 if($event_start_time_s<$s_rec_s){
-
+    $start_old_reverse=$s_rec_s - $event_start_time_s;
+    $start_old=date("H:i:s", mktime(0,0, 3600-$start_old_reverse-$prerecord_interval));//Начало события в старом куске
     //ffmpeg с олд
+    system("start-stop-daemon -Sbmp /var/www/html/modules/record/devices/'$cam_name'/pid_event1 -x /usr/bin/ffmpeg -- -i '$path_res'/record_old.avi -ss '$start_old_reverse' -vcodec copy -acodec copy '$video_storage'/'$id_event'_1.avi > /var/www/html/modules/event/event_mod.log");
+    unlink("/var/www/html/modules/record/devices/'.$cam_name.'/pid_event1");
     //ff с текущей
+    $final_event_time=date("H:i:s", mktime(0,0, $event_end_time_s - $s_rec_s + $postrecord_interval));
+    system("start-stop-daemon -Sbmp /var/www/html/modules/record/devices/'$cam_name'/pid_event2 -x /usr/bin/ffmpeg -- -i '$path_res'/record_old.avi -t '$final_event_time' -vcodec copy -acodec copy '$video_storage'/'$id_event'_2.avi > /var/www/html/modules/event/event_mod.log");
+    unlink("/var/www/html/modules/record/devices/'.$cam_name.'/pid_event2");
     //сплит
+    system("start-stop-daemon -Sbmp /var/www/html/modules/record/devices/'$cam_name'/pid_event_spl -x /usr/bin/ffmpeg -- -i concat: \"'$path_res'/'$id_event'_1.avi|'$path_res'/'$id_event'_2.avi\" -c copy '$video_storage'/'$id_event'.avi > /var/www/html/modules/event/event_mod.log");
     //system("start-stop-daemon -Sbmp /var/www/html/modules/record/devices/'$cam_name'/pid_event -x /usr/bin/ffmpeg -- -ss '$start' '$duration' -i '$path_res'/record.avi -vcodec copy '$video_storage'/'$id_event'.avi > /var/www/html/modules/event/event_mod.log");
-    //unlink("/var/www/html/modules/record/devices/'.$cam_name.'/pid_event");
+    unlink("/var/www/html/modules/record/devices/'.$cam_name.'/pid_event_spl");
+    unlink("/var/www/html/modules/record/devices/'.$cam_name.'/'.$id_event.'_1.avi");
+    unlink("/var/www/html/modules/record/devices/'.$cam_name.'/'.$id_event.'_2.avi");
 }else{
     //фф с текущей
-    $start=date("H:i:s", mktime(0,0,$event_start_time_s - $s_rec_s));
-    $duration=date("H:i:s", mktime(0,0,$event_end_time_s - $event_start_time_s));
-    system("start-stop-daemon -Sbmp /var/www/html/modules/record/devices/'$cam_name'/pid_event -x /usr/bin/ffmpeg -- -i '$path_res'/record.avi -ss '$start' '$duration' -vcodec copy '$video_storage'/'$id_event'.avi > /var/www/html/modules/event/event_mod.log");
+    $start=date("H:i:s", mktime(0,0,$event_start_time_s - $s_rec_s - $prerecord_interval));
+    $duration=date("H:i:s", mktime(0,0,$event_end_time_s - $event_start_time_s + $prerecord_interval + $postrecord_interval));
+    system("start-stop-daemon -Sbmp /var/www/html/modules/record/devices/'$cam_name'/pid_event -x /usr/bin/ffmpeg -- -i '$path_res'/record.avi -ss '$start' -t '$duration' -vcodec copy -acodec copy '$video_storage'/'$id_event'.avi > /var/www/html/modules/event/event_mod.log");
     unlink("/var/www/html/modules/record/devices/'.$cam_name.'/pid_event");
     //$d4=date("H:i:s", mktime(0,0,$D2-$D1));
 }
--- test.php
+++ test.php
@@ -3,10 +3,13 @@ $d1="2016-09-27 14:59:40";
 $d2="2016-09-27 15:00:10";
 $D1=  strtotime($d1);
 $D2= strtotime($d2);
-$delta_d=$D2-$D1;
-$mk=mktime(0,0,$D2-$D1);
+//$delta_d=$D2-$D1;
+
 $d4=date("H:i:s", mktime(0,0,$D2-$D1));
-echo $d4;
+$d5=$D2-$D1;
+$hr=3600;
+$d6=date("H:i:s", mktime(0,0, $hr-$d5));
+echo $d6;
 //$d3=$D2-$D1;
 //echo $d3;
 //echo $D1.'/';
